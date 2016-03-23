60e47aab1ef93e68d6d6448e63f81cbab7d33c31
lipeng <lipeng@baicheng.com>
2016-03-23 10:59:59 +0000

    middleware

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+class PageController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('page');
+//        $this->middleware('article');
+    }
+
+    public function getIndex(Request $request)
+    {
+//        $request->session()->flush();
+        $request->session()->push('usernames',$request->input('name'));
+
+        $parame = $request->all();
+        echo "<pre>";
+        print_r($parame);
+    }
+    public function getShow(Request $request)
+    {
+        $parames = $request->old();
+        echo '<pre>';
+        echo session('username1','lipeng');
+       $usernames = $request->session()->get('usernames');
+        var_dump($usernames);
+
+    }
+}
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,5 +48,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'page'=>\App\Http\Middleware\PageMiddleware::class,
+        'article'=>\App\Http\Middleware\ArticleMiddleware::class,
     ];
 }
